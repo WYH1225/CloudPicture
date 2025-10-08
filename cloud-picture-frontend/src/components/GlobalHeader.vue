bef3d29fff63e185898408b9354f1455ae46e9c2
yehua wu <1966209573@qq.com>
2025-10-08 21:18:13 +0800

    前端初始化ing...

---
+++ cloud-picture-frontend/src/components/GlobalHeader.vue
@@ -0,0 +1,66 @@
+<template>
+    <div id="globalHeader">
+        <a-row :wrap="false">
+            <a-col flex="200px">
+                <router-link to="/">
+                    <div class="title-bar">
+                        <img src="../assets/logo.png" alt="logo" class="logo" />
+                        <div class="title">云图库</div>
+                    </div>
+                </router-link>
+            </a-col>
+            <a-col flex="auto">
+                <a-menu v-model:selectedKeys="current" mode="horizontal" :items="items" />
+            </a-col>
+            <a-col flex="120px">
+                <div class="user-login-status">
+                    <a-button type="primary" href="/user/login">登录</a-button>
+                </div>        
+            </a-col>
+        </a-row>
+        
+        
+        
+    </div>
+</template>
+<script lang="ts" setup>
+import { h, ref } from 'vue';
+import { HomeOutlined } from '@ant-design/icons-vue';
+import { MenuProps } from 'ant-design-vue';
+const current = ref<string[]>(['mail']);
+const items = ref<MenuProps['items']>([
+    {
+        key: '/',
+        icon: () => h(HomeOutlined),
+        label: '主页',
+        title: '主页',
+    },
+    {
+        key: '/about',
+        label: '关于',
+        title: '关于',
+    },
+    {
+        key: 'others',
+        label: h('a', {href: 'https://baidu.com', target: '_blank'}, '百度'),
+        title: '百度',
+    },
+]);
+</script>
+
+<style scoped>
+#globalHeader .title-bar {
+    display: flex;
+    align-items: center;
+}
+
+.title {
+    color: black;
+    font-size: 18px;
+    margin-left: 16px;
+}
+
+.logo {
+    height: 48px;
+}
+</style>
---
+++ cloud-picture-frontend/src/layouts/BasicLayout.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="basicLayout">
+        <a-layout style="min-height: 100vh">
+            <a-layout-header class="header">
+                <GlobalHeader />
+            </a-layout-header>
+            <a-layout-content class="content">
+                <router-view />
+            </a-layout-content>
+            <a-layout-footer class="footer">
+                云图库 by wyh
+            </a-layout-footer>
+        </a-layout>
+    </div>
+</template>
+
+<script setup lang="ts">
+import GlobalHeader from '@/components/GlobalHeader.vue';
+</script>
+
+<style scoped>
+#basicLayout .header {
+    margin-inline: 20px;
+    background: white;
+    color: unset;
+    margin-bottom: 16px;
+}
+
+#basicLayout .content {
+    padding: 40px;
+    background: linear-gradient(to right, #fefefe, #fff);
+    margin-bottom: 20px;
+}
+
+#basicLayout .footer {
+    background-color: #efefef;
+    padding: 16px;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    text-align: center;
+}
+</style>
