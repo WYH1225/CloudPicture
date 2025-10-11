98f62e33c951decf48719a8ed545f51764ba893b
yehua wu <1966209573@qq.com>
2025-10-11 22:35:24 +0800

    图片模块（后端上传文件completed）

---
+++ cloud-picture-backend/src/main/java/com/pic/cloudpicturebackend/controller/FileController.java
@@ -0,0 +1,58 @@
+package com.pic.cloudpicturebackend.controller;
+
+import com.pic.cloudpicturebackend.annotation.AuthCheck;
+import com.pic.cloudpicturebackend.common.BaseResponse;
+import com.pic.cloudpicturebackend.common.ResultUtils;
+import com.pic.cloudpicturebackend.constant.UserConstant;
+import com.pic.cloudpicturebackend.exception.BusinessException;
+import com.pic.cloudpicturebackend.exception.ErrorCode;
+import com.pic.cloudpicturebackend.manager.CosManager;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.io.File;
+
+@RestController
+@RequestMapping("/file")
+@Slf4j
+public class FileController {
+
+    @Resource
+    private CosManager cosManager;
+
+    /**
+     * 测试上传文件
+     * @param multipartFile
+     * @return
+     */
+    @PostMapping("/test/upload")
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    public BaseResponse<String> testUploadFile(@RequestPart("file") MultipartFile multipartFile) {
+        // 文件目录
+        String fileName = multipartFile.getOriginalFilename();
+        String filePath = String.format("/test/%s", fileName);
+        File file = null;
+        try {
+            // 上传文件
+            file = File.createTempFile(filePath, null);
+            multipartFile.transferTo(file);
+            cosManager.putObject(filePath, file);
+            return ResultUtils.success(filePath);
+        } catch (Exception e) {
+            log.error("上传失败, filePath = {}", filePath);
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "上传失败");
+        } finally {
+            if (file != null) {
+                boolean delete = file.delete();
+                if (!delete) {
+                    log.error("删除临时文件失败, filePath = {}", filePath);
+                }
+            }
+        }
+    }
+}
