2e9cef5f0ffa5c07e2da8cd613f8da4fb3b7b149
yehua wu <1966209573@qq.com>
2025-10-16 15:14:56 +0800

    用户传图-url上传图片-前端-completed

--- cloud-picture-frontend/src/api/pictureController.ts
+++ cloud-picture-frontend/src/api/pictureController.ts
@@ -130,7 +130,22 @@ export async function updatePictureUsingPost(
   })
 }
 
-/** uploadPicture POST /api/picture/upload */
+/** uploadPictureByUrl POST /api/picture/upload */
+export async function uploadPictureByUrlUsingPost(
+  body: API.PictureUploadRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponsePictureVO_>('/api/picture/upload', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** uploadPicture POST /api/picture/upload/url */
 export async function uploadPictureUsingPost(
   // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
   params: API.uploadPictureUsingPOSTParams,
@@ -160,7 +175,7 @@ export async function uploadPictureUsingPost(
     }
   })
 
-  return request<API.BaseResponsePictureVO_>('/api/picture/upload', {
+  return request<API.BaseResponsePictureVO_>('/api/picture/upload/url', {
     method: 'POST',
     params: {
       ...params,
